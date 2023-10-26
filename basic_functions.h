f52ffa6c4b71de72f6008e83cd79c3ebbb4255aa
Vishal-Singh27 <vskota273@gmail.com>
2023-10-27 00:32:04 +0530

    Added more features to basic_functions.h

--- basic_functions.h
+++ basic_functions.h
@@ -1,16 +1,91 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int get_int(char* txt);
+int get_numfromstart(char *txt, int start);
+int get_numfromstarttoend(char *txt, int start, int end);
 char *get_string(char *txt);
 void print_arr(int arrlen, int *arr);
 
 int get_int(char *txt) 
 {
-    int num;
-    printf("%s", txt);
-    scanf("%i", &num);
+    char numstr[20];
 
-    return num;
+    loop:
+    printf("%s", txt);
+    scanf("%s", numstr);
+
+    for (int i = 0; i < strlen(numstr); i++) {
+        if (isdigit(numstr[i]) == 0) {
+            printf("\nEnter an integer!!\n\n");
+            goto loop;
+        }
+    }
+
+    return atoi(numstr);
+}
+
+int get_numfromstart(char *txt, int start) {
+    int num;
+    char numstr[20];
+
+    loop:
+    printf("%s", txt);
+    scanf("%s", numstr);
+
+    if (isdigit(numstr[0]) == 0 && numstr[0] != '-') {
+        printf("\nEnter a number greater than %i!!\n\n", start);
+        goto loop;
+    }
+
+    for (int i = 0; i < strlen(numstr); i++) {
+        if (isdigit(numstr[i]) == 0) {
+            printf("\nEnter a number greater than %i!!\n\n", start);
+            goto loop;
+        }
+    }
+
+    num = atoi(numstr);
+    
+    if (num < start) {
+        printf("\nEnter a number greater than %i!!\n\n", start);
+        goto loop;
+    }
+
+    return atoi(numstr);
+}
+
+int get_numfromstarttoend(char *txt, int start, int end) {
+    int num;
+    char numstr[20];
+
+    loop:
+    printf("%s", txt);
+    scanf("%s", numstr);
+
+    if (isdigit(numstr[0]) == 0 && numstr[0] != '-') {
+        printf("\nEnter a number greater than %i and less than %i!!\n\n", start, end);
+        goto loop;
+    }
+
+    for (int i = 0; i < strlen(numstr); i++) {
+        if (isdigit(numstr[i]) == 0) {
+            printf("\nEnter a number greater than %i and less than %i!!\n\n", start, end);
+            goto loop;
+        }
+    }
+
+    num = atoi(numstr);
+    
+    if (num < start || num > end) {
+        printf("\nEnter a number greater than %i and less than %i!!\n\n", start, end);
+        goto loop;
+    }
+
+    return atoi(numstr);
 }
 
 char *get_string(char *txt)
