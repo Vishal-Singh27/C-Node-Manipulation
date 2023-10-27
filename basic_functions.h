2fe061a95884e512efa93182e46a9ae8ecaeaf87
Vishal-Singh27 <vskota273@gmail.com>
2023-10-27 22:22:21 +0530

    Edited basic_functions

--- basic_functions.h
+++ basic_functions.h
@@ -1,34 +1,88 @@
+// Including necessary libraries
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
+// Declaring the functions first
+bool check_int(char *numstr);
 int get_int(char* txt);
-int get_numfromstart(char *txt, int start);
-int get_numfromstarttoend(char *txt, int start, int end);
+int get_intfromstart(char *txt, int start);
+int get_intfromstarttoend(char *txt, int start, int end);
+bool check_float(char *numstr);
+float get_float(char *txt);
 char *get_string(char *txt);
+char get_char(char *txt);
 void print_arr(int arrlen, int *arr);
 
+// This function checks if a string is a valid int
+bool check_int(char *numstr) {
+    int stringlen = strlen(numstr);
+
+    if (isdigit(numstr[0]) == 0 && numstr[0] != '-') {
+        return false;
+    }
+
+    for (int i = 1; i < stringlen; i++) {
+        if (isdigit(numstr[i]) == 0) {
+            return false;
+        }
+    }
+
+    // If program comes to this point then it is an integer
+    return true;
+}
+
+// Helps in getting input of a float
+bool check_float(char *numstr) {
+    int stringlen = strlen(numstr);
+    uint16_t dotcount = 0;
+
+    if (isdigit(numstr[0]) == 0 && numstr[0] != '-' && numstr[0] != '.') {
+        return false;
+    }
+
+    if (numstr[0] == '.') {
+        dotcount = 1;
+    }
+
+    for (int i = 1; i < stringlen; i++) {
+        if (isdigit(numstr[i]) == 0 && (dotcount == 1 && numstr[i] == '.')) {
+            return false;
+        }
+
+        else if (numstr[i] == '.') {
+            dotcount = 1;
+        }
+    }
+
+    // If program comes to this point then it is a float
+    return true;
+}
+
+// This function helps in getting input of an integer
 int get_int(char *txt) 
 {
+    // First the string which will first be used to take the number as a string
     char numstr[20];
 
+    // The goto type loop that goes on till user inputs an integer
     loop:
-    printf("%s", txt);
+    printf("%s", txt); 
     scanf("%s", numstr);
 
-    for (int i = 0; i < strlen(numstr); i++) {
-        if (isdigit(numstr[i]) == 0) {
-            printf("\nEnter an integer!!\n\n");
-            goto loop;
-        }
+
+    if (check_int(numstr) == false) {
+        printf("\nNot an integer!!\n\n");
+        goto loop;
     }
 
     return atoi(numstr);
 }
 
-int get_numfromstart(char *txt, int start) {
+// Getting an int from a specific starting number
+int get_intfromstart(char *txt, int start) {
     int num;
     char numstr[20];
 
@@ -36,16 +90,9 @@ int get_numfromstart(char *txt, int start) {
     printf("%s", txt);
     scanf("%s", numstr);
 
-    if (isdigit(numstr[0]) == 0 && numstr[0] != '-') {
-        printf("\nEnter a number greater than %i!!\n\n", start);
+    if (check_int(numstr) == false) {
+        printf("\nNot an integer!!\n\n");
         goto loop;
-    }
-
-    for (int i = 0; i < strlen(numstr); i++) {
-        if (isdigit(numstr[i]) == 0) {
-            printf("\nEnter a number greater than %i!!\n\n", start);
-            goto loop;
-        }
     }
 
     num = atoi(numstr);
@@ -58,7 +105,8 @@ int get_numfromstart(char *txt, int start) {
     return atoi(numstr);
 }
 
-int get_numfromstarttoend(char *txt, int start, int end) {
+// Getting an int from a specific starting number and an ending number
+int get_intfromstarttoend(char *txt, int start, int end) {
     int num;
     char numstr[20];
 
@@ -66,16 +114,9 @@ int get_numfromstarttoend(char *txt, int start, int end) {
     printf("%s", txt);
     scanf("%s", numstr);
 
-    if (isdigit(numstr[0]) == 0 && numstr[0] != '-') {
-        printf("\nEnter a number greater than %i and less than %i!!\n\n", start, end);
+    if (check_int(numstr) == false) {
+        printf("\nNot an integer!!\n\n");
         goto loop;
-    }
-
-    for (int i = 0; i < strlen(numstr); i++) {
-        if (isdigit(numstr[i]) == 0) {
-            printf("\nEnter a number greater than %i and less than %i!!\n\n", start, end);
-            goto loop;
-        }
     }
 
     num = atoi(numstr);
@@ -88,6 +129,27 @@ int get_numfromstarttoend(char *txt, int start, int end) {
     return atoi(numstr);
 }
 
+// This function helps in getting input of an float
+float get_float(char *txt) 
+{
+    // First the string which will first be used to take the number as a string
+    char numstr[20];
+
+    // The goto type loop that goes on till user inputs an integer
+    loop:
+    printf("%s", txt); 
+    scanf("%s", numstr);
+
+
+    if (check_float(numstr) == false) {
+        printf("\nNot a float!!\n\n");
+        goto loop;
+    }
+
+    return atof(numstr);
+}
+
+// This helps in getting input of a string
 char *get_string(char *txt)
 {
     char *str = calloc(sizeof(char *), 200);
@@ -98,6 +160,24 @@ char *get_string(char *txt)
     return str;
 }
 
+// This helps in getting input of a string
+char get_char(char *txt)
+{
+    char *str = calloc(sizeof(char *), 3);
+
+    loop:
+    printf("%s", txt);
+    scanf("%s", str);
+
+    if (strlen(str) > 1) {
+        printf("\nJust enter one character!!\n\n");
+        goto loop;
+    }
+
+    return str[0];
+}
+
+// This helps in printing an array
 void print_arr(int arrlen, int *arr) {
     printf("[");
     for (int i = 0; i < arrlen; i++) {
